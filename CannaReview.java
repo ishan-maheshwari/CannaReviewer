c6e79fb2c29e9c2702092a032dce16fae9f14de0
Alan Sato <halfcreative@gmail.com>
2016-10-24 14:07:55 -0400

    Added CannaReview.Java
    The Beginning of something great.

---
+++ CannaReview.java
@@ -0,0 +1,31 @@
+
+/**
+ * 
+ * 
+ * @author (name of author(s)) 
+ * @version (date submitted)
+ * 
+ *
+ * NOTES:
+ *
+ */
+public class CannaReview
+{
+    //*** fields (if any)
+    double vnum = 1.0;
+    //*** methods
+
+
+    /**
+     * Performs main action for this class.
+     * (Called automatically by Java.)
+     */
+    public void main(String [] args)
+    {
+      // put your code here!!!
+      System.out.println("Hello");
+      System.out.println("Welcome to CannaReview v" + vnum);
+
+
+    } // end main() method
+} // end class
